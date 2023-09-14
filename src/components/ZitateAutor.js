086ff580450f539ebd98ce6c675fd38e6671c047
Sebastian Wineck <mail@swineck.de>
2023-09-14 09:54:55 +0200

    slight changes

--- src/components/ZitateAutor.js
+++ src/components/ZitateAutor.js
@@ -7,10 +7,10 @@ const {zitat, generateRandomQuote} = props;
 return(
 <div id="quote-box">
 <p id="quote" className="title">Quote: </p>
-<div id="quote-content" data-testid="quote" className="content">{zitat.zitat}</div>
-<p id="author" className="title">Author: </p>
-<div id="author-content" data-testid="author" className="content">{zitat.autor}</div>
-<button id="generate-quote" onClick={generateRandomQuote}>Generate Quote</button>
+<div id="text" data-testid="quote" className="content">{zitat.zitat}</div>
+<p className="title">Author: </p>
+<div id="author" data-testid="author" className="content">{zitat.autor}</div>
+<button id="new-quote" onClick={generateRandomQuote}>Generate Quote</button>
 <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${zitat.zitat} - ${zitat.autor}`} target="_blank" rel="noopener noreferrer">Tweet this Quote</a>
 </div>
 
